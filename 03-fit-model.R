108bd217bd2f69481bdf1405e96c293d23a1d121
Quang Huynh <quang@bluematterscience.com>
2024-05-24 13:41:29 -0700

    Add profile and retrospective

--- 03-fit-model.R
+++ 03-fit-model.R
@@ -16,7 +16,7 @@ map_recdist_rs[2, 2] <- 1
 
 pars <- make_parameters(
   dat,
-  start = list(log_recdist_rs = log_recdist_rs, R0_s = c(2000, 20000)),
+  start = list(log_recdist_rs = log_recdist_rs, R0_s = c(4000, 400)),
   map = list(log_recdist_rs = factor(map_recdist_rs)),
   est_mov = "gravity_fixed"
 )
@@ -39,4 +39,81 @@ saveRDS(fit, file = "fit/model_April2024.rds")
 
 fit <- readRDS("fit/model_April2024.rds")
 report(fit, amov = 1, dir = "fit", filename = "preliminary_fit")
+
+r <- MARS::retrospective(fit, cores = 5)
+saveRDS(r, file = "fit/ret_April2024.rds")
+
+Rprof <- expand.grid(
+  seq(1500, 3000, 250),
+  seq(50, 90, 10)
+)
+
+p <- profile(
+  fit,
+  p1 = "R0_s[1]",
+  p2 = "R0_s[2]",
+  v1 = seq(1000, 5000, 500),
+  v2 = seq(30, 90, 10),
+  cores = 10
+)
+saveRDS(p, file = "fit/R0prof_April2024.rds")
+
+p1 <- profile(
+  fit,
+  p1 = "R0_s[1]",
+  v1 = seq(1000, 5000, 250),
+  cores = 10
+)
+saveRDS(p1, file = "fit/R01prof_April2024.rds")
+
+
+prof <- readRDS("r1prof.rds")
+prof[[9]] <- prof[[10]]
+prof[[9]][[1]][] <- prof[[9]][[2]][] <- NA
+prof2 <- lapply(prof, bind_rows) %>% bind_rows()
+
+
+R0_1 <- seq(1000, 5000, 250)
+
+p2 <- profile(
+  fit,
+  p1 = "R0_s[2]",
+  v1 = seq(30, 90, 5),
+  cores = 10
+)
+saveRDS(p2, file = "fit/R02prof_April2024.rds")
+
+
+
+# Joint profile over R0 for both EBFT/WBFT
+p <- readRDS("fit/R0prof_April2024.rds")
+
+plot(p, levels = seq(0, 200, 10), xlab = "EBFT R0", ylab = "WBFT R0")
+
+plot(p, levels = seq(0, 200, 10), xlab = "EBFT R0", ylab = "WBFT R0", component = 'loglike_I_ymi')
+plot(p, levels = seq(0, 200, 10), xlab = "EBFT R0", ylab = "WBFT R0", component = 'loglike_CAL_ymfr')
+plot(p, levels = seq(0, 200, 10), xlab = "EBFT R0", ylab = "WBFT R0", component = 'loglike_tag_mov_ymars')
+plot(p, levels = seq(0, 200, 10), xlab = "EBFT R0", ylab = "WBFT R0", component = 'loglike_SC_ymafr')
+plot(p, levels = seq(0, 200, 10), xlab = "EBFT R0", ylab = "WBFT R0", component = 'logprior_rdev_ys')
+
+
+# Profile over R0 for EBFT
+p <- readRDS("fit/R01prof_April2024.rds")
+plot(p, xlab = "EBFT R0")
+
+plot(p, xlab = "EBFT R0", component = 'loglike_I_ymi')
+plot(p, xlab = "EBFT R0", component = 'loglike_CAL_ymfr')
+plot(p, xlab = "EBFT R0", component = 'loglike_tag_mov_ymars')
+plot(p, xlab = "EBFT R0", component = 'loglike_SC_ymafr')
+plot(p, xlab = "EBFT R0", component = 'logprior_rdev_ys')
+
+# Profile over R0 for WBFT
+p <- readRDS("fit/R02prof_April2024.rds")
+plot(p, xlab = "WBFT R0")
+
+plot(p, xlab = "WBFT R0", component = 'loglike_I_ymi')
+plot(p, xlab = "WBFT R0", component = 'loglike_CAL_ymfr')
+plot(p, xlab = "WBFT R0", component = 'loglike_tag_mov_ymars')
+plot(p, xlab = "WBFT R0", component = 'loglike_SC_ymafr')
+plot(p, xlab = "WBFT R0", component = 'logprior_rdev_ys')
 
